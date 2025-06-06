562229e9bcd75193f85abe54890f2abbc8bff914
Mauro Luque <mauroluque72@gmail.com>
2025-06-06 13:58:51 -0300

    add docker and logger

--- src/app.js
+++ src/app.js
@@ -1,25 +1,56 @@
-import express from 'express';
-import mongoose from 'mongoose';
-import cookieParser from 'cookie-parser';
-import { configDotenv } from 'dotenv';
-
-import usersRouter from './routes/users.router.js';
-import petsRouter from './routes/pets.router.js';
-import adoptionsRouter from './routes/adoption.router.js';
-import sessionsRouter from './routes/sessions.router.js';
+import express from "express";
+import mongoose from "mongoose";
+import cookieParser from "cookie-parser";
+import { configDotenv } from "dotenv";
+import { faker } from "@faker-js/faker";
+import usersRouter from "./routes/users.router.js";
+import petsRouter from "./routes/pets.router.js";
+import adoptionsRouter from "./routes/adoption.router.js";
+import sessionsRouter from "./routes/sessions.router.js";
+import { logger } from "./utils/logger.js";
+import cluster from "cluster";
+import { cpus } from "os";
 
 configDotenv();
 
 const app = express();
-const PORT = process.env.PORT||8080;
-const connection = mongoose.connect(process.env.MONGO_URI)
+const PORT = process.env.PORT || 8080;
+const connection = mongoose.connect(process.env.MONGO_URI);
 
 app.use(express.json());
 app.use(cookieParser());
 
-app.use('/api/users',usersRouter);
-app.use('/api/pets',petsRouter);
-app.use('/api/adoptions',adoptionsRouter);
-app.use('/api/sessions',sessionsRouter);
+app.use("/api/users", usersRouter);
+app.use("/api/pets", petsRouter);
+app.use("/api/adoptions", adoptionsRouter);
+app.use("/api/sessions", sessionsRouter);
 
-app.listen(PORT,()=>console.log(`Listening on ${PORT}`))
+app.get("/api/test/user", (req, res) => {
+  let first_name = faker.person.firstName();
+  let last_name = faker.person.lastName();
+  let email = faker.internet.email();
+  let password = faker.internet.password();
+
+  res.send({ first_name, last_name, email, password });
+});
+
+if (cluster.isPrimary) {
+  for (let i = 0; i < cpus().length; i++) {
+    cluster.fork();
+  }
+
+  cluster.on("exit", (worker) => {
+    logger.error(
+      "exit process: " +
+        worker.process.pid +
+        " " +
+        new Date().toLocaleDateString()
+    );
+    cluster.fork();
+  });
+}
+if (cluster.isWorker) {
+  app.listen(PORT, () =>
+    logger.info(`Process ${process.pid} Listening on ${PORT}`)
+  );
+}
